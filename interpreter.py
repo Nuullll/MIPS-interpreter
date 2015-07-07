046c3a0a0358ae225b38cf5fc9d887fc8535f4c9
Nuullll <vfirst218@gmail.com>
2015-07-08 01:14:59 +0800

    dec2bin()

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,14 @@
 # -*- coding: UTF-8 -*-
 # interpreter.py
 
+def dec2bin(num, bits):
+    '''convert decimal number to bin_str with n bits'''
+    if num >= 0:
+        return ("{0:0%db}" % bits).format(num)
+    else:
+        return ("{0:0%db}" % bits).format(2**bits + num)
+
+
 def parseRegister(reg_str):
     '''e.g. reg_str = "$zero", will be parsed to "00000"'''
     regs = ['$zero', '$at', '$v0', '$v1', '$a0', '$a1', '$a2', '$a3',
@@ -8,10 +16,7 @@ def parseRegister(reg_str):
             '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7',
             '$t8', '$t9', '$k0', '$k1', '$gp', '$sp', '$fp', '$ra']
     try:
-        bin_str = bin(regs.index(reg_str))[2:]
-        bin_str = '0' * (5 - len(bin_str)) + bin_str
-    except IndexError:
+        return dec2bin(regs.index(reg_str), 5)
+    except ValueError:
         print('undefined register:', reg_str)
-
-    return bin_str
 
