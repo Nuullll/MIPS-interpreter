cb1aa04e5a00353af532fc3e4ed35e545500f541
Nuullll <vfirst218@gmail.com>
2015-07-08 10:15:28 +0800

    hex2bin() and parseLwSw

--- interpreter.py
+++ interpreter.py
@@ -3,12 +3,41 @@
 
 import sys
 
-def dec2bin(num, bits):
+def radix(num_str):
+    if num_str.lower().startswith('0x'):
+        return 16
+    else:
+        return 10
+
+
+def dec2bin(num_str, bits):
     '''convert decimal number to bin_str with n bits'''
+    num = int(num_str)
     if num >= 0:
         return ("{0:0%db}" % bits).format(num)
     else:
         return ("{0:0%db}" % bits).format(2**bits + num)
+
+
+def hex2bin(num_str, bits):
+    d = {'0':'0000', '1':'0001', '2':'0010', '3':'0011',
+         '4':'0100', '5':'0101', '6':'0110', '7':'0111',
+         '8':'1000', '9':'1001', 'a':'1010', 'b':'1011',
+         'c':'1100', 'd':'1101', 'e':'1110', 'f':'1111'}
+    bin_str = ''
+    try:
+        for ch in num_str:
+            bin_str += d[ch.lower()]
+    except KeyError:
+        print('illegal hex input:', num_str)
+        raise
+
+    if len(bin_str) < bits:
+        bin_str = bin_str[0] * (bits - len(bin_str)) + bin_str
+    else:
+        bin_str = bin_str[-bits:]
+
+    return bin_str
 
 
 def parseRegister(reg_str):
@@ -21,6 +50,7 @@ def parseRegister(reg_str):
         return dec2bin(regs.index(reg_str), 5)
     except ValueError:
         print('undefined register:', reg_str)
+        raise
 
 
 def delComment(line_str):
@@ -34,6 +64,23 @@ def delComment(line_str):
 def isLabel(non_comment_line):
     '''return whether this line is a label'''
     return ':' in non_comment_line
+
+
+def parseInstruction(instruction):
+    '''parse instruction to bin_str'''
+    l = instruction.split()
+    op = l.pop(0)
+    l = [item.rstrip(',') for item in l]
+    if op in ['lw', 'sw']:
+        return parseLwSw(op, l)
+
+
+def parseLwSw(op, argv):
+    '''lw rt, offset(rs)
+    sw rt, offset(rs)'''
+    rt_str = parseRegister[argv[0]]
+    offset = argv[1][:argv[1].index('(')]
+    rs = argv[1][argv[1].index('(')+1:argv[1].index(')')]
 
 
 if __name__ == '__main__':
