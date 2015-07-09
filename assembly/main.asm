40f2fd583f73a318ef58341c255780437e64e4d6
Nuullll <vfirst218@gmail.com>
2015-07-09 12:43:34 +0800

    loop1

--- assembly/main.asm
+++ assembly/main.asm
@@ -1,8 +1,22 @@
 # main.asm
 
-    j GCD           # reset
-    j Break         # break
-    j Exception     # exception
+    j       GCD             # reset
+    j       Break           # break
+    j       Exception       # exception
 
 GCD:
+    # $a0 = operand0, $a1 = operand1
+    addi    $t0, $zero, 0   # t0 counts trailing zeros of operand0
+    addi    $t1, $zero, 0   # t1 counts trailing zeros of operand1
+    addi    $t2, $zero, 1   # t2 = 1
+
+Loop1:
+    and     $t3, $a0, $t2   # t3 = a0 & 1, if a0 is even then t3 = 0
+    bne     $t3, $zero, Skip1   # jump to Skip1 if a0 is odd
+    addi    $t0, $t0, 1     # t0 += 1
+    srl     $a0, $a0, 1     # a0 /= 2
+    j       Loop1
+
+Skip1:
     
+
