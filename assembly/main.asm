ee5de97687d11dd60f30d8f11693dafbd6048fa4
Nuullll <vfirst218@gmail.com>
2015-07-10 01:55:53 +0800

    display via scaning

--- assembly/main.asm
+++ assembly/main.asm
@@ -5,6 +5,39 @@
     j       Exception       # exception
 
 Reset:
+    addi    $gp, $zero, 0       # gp = 0
+    addi    $t0, $zero, 0x0040  # 0
+    sw      $t0, 0($gp)
+    addi    $t0, $zero, 0x0079  # 1
+    sw      $t0, 4($gp)
+    addi    $t0, $zero, 0x0024  # 2
+    sw      $t0, 8($gp)
+    addi    $t0, $zero, 0x0030  # 3
+    sw      $t0, 16($gp)
+    addi    $t0, $zero, 0x0019  # 4
+    sw      $t0, 20($gp)
+    addi    $t0, $zero, 0x0012  # 5
+    sw      $t0, 24($gp)
+    addi    $t0, $zero, 0x0002  # 6
+    sw      $t0, 28($gp)
+    addi    $t0, $zero, 0x0078  # 7
+    sw      $t0, 32($gp)
+    addi    $t0, $zero, 0x0000  # 8
+    sw      $t0, 36($gp)
+    addi    $t0, $zero, 0x0010  # 9
+    sw      $t0, 40($gp)
+    addi    $t0, $zero, 0x0008  # A
+    sw      $t0, 44($gp)
+    addi    $t0, $zero, 0x0003  # b
+    sw      $t0, 48($gp)
+    addi    $t0, $zero, 0x0046  # C
+    sw      $t0, 52($gp)
+    addi    $t0, $zero, 0x0021  # d
+    sw      $t0, 56($gp)
+    addi    $t0, $zero, 0x0006  # E
+    sw      $t0, 60($gp)
+    addi    $t0, $zero, 0x000e  # F
+    sw      $t0, 64($gp)
 
     lui     $s2, 0x4000         # addr of timer: 0x40000000
     sw      $zero, 8($s2)       # TCON = 0
@@ -72,9 +105,9 @@ Scan:
     andi    $t1, $t1, 0x000f    # t1 = {0...0, digi[11:8]}
     sll     $t1, $t1, 1         # next digi
     addi    $t2, $zero, 0x0010  # t2 = {0...1, 0000}  fourth digi << 1
-    bne     $t1, $t2, Display
+    bne     $t1, $t2, Select
     addi    $t1, $zero, 0x0001  # first digi
-Display:
+Select:
     addi    $t3, $zero, 0x0001  # first digi
     addi    $t4, $zero, 0x0002  # second digi
     addi    $t5, $zero, 0x0004  # third digi
@@ -84,7 +117,31 @@ Display:
     beq     $t1, $t5, Digi3
     beq     $t1, $t6, Digi4
 Digi1:
-    
+    srl     $t2, $s0, 4
+    j       Display
+Digi2:
+    andi    $t2, $s0, 0x000f
+    j       Display
+Digi3:
+    srl     $t2, $s1, 4
+    j       Display
+Digi4:
+    andi    $t2, $s1, 0x000f
+    j       Display
+Display:
+    sll     $t2, $t2, 2
+    add     $t3, $gp, $t2
+    lw      $t2, 0($t3)
+    sll     $t1, $t1, 8
+    add     $t0, $t1, $t2       # t0 = digi[11:0]
+    sw      $t0, 20($s2)        # digi = t0
+
+    lw      $t0, 8($s2)         # t0 = TCON
+    addi    $t1, $zero, 0x0002
+    or      $t0, $t0, $t1       # t0 |= 0x00000002
+    sw      $t0, 8($s2)         # TCON = t0
+
+    jr      $k0                 # jump to $26
 
 Exception:
 
